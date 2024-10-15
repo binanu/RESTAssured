7f3f56d7776a5bcf2c77c085e8384c98341560da
binanu <testbiniyam@gmail.com>
2024-10-15 00:05:56 -0400

    Adding First RESTAssured test

---
+++ src/test/java/tests/TestsExamples.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class TestsExamples {
+	@Test
+public void test_1() {
+		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+		System.out.println("status code = " + response.getStatusCode());
+		Assert.assertEquals(response.getStatusCode(), 201);
+		System.out.println("response time = " + response.getTime());
+		System.out.println("response body: ... " + response.getBody().asPrettyString());
+		System.out.println("status line: " + response.getStatusLine());
+		System.out.println("header: " + response.getHeader("content-type"));
+		
+	
+}
+}
